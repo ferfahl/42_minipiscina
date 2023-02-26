8531247b427c8d8337325841f85ae7b7b8184f58
ferfahl <fe.alves.fahl@gmail.com>
2023-02-25 21:46:13 -0300

    Cleaning up for git stats

--- D04/ex00/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Ex00Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ex00'
--- D04/ex00/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from ex00 import views
-
-urlpatterns = [
-    path('', views.HomePageView.as_view(), name='first_page'), # Notice the URL has been named
-]
